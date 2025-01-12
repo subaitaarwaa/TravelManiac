0a221cccb7833de8cb9793f07ca27cfd7c4cfc67
Kuro-Akuma <medhazamanofficial@gmail.com>
2025-01-13 02:13:32 +0800

    Add files via upload

---
+++ src/main/java/utils/FileHandler.java
@@ -0,0 +1,14 @@
+package utils;
+
+import models.Trip;
+import models.Activity;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class FileHandler {
+    public static ArrayList<Trip> loadTrips() {
+        // Dummy implementation for file loading
+        return new ArrayList<>();
+    }
+}
